6acf144dee325ee6e9f7ffb62cf78ce5faed3437
Ben Hong <bencodezen@gmail.com>
2018-04-04 17:59:55 -0400

    refactor: Remove boilerplate content

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,49 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div id="app" :class="$style.app">
+    <nav id="nav" :class="$style.nav">
+      <ul>
+        <li><a href="#">Introduction</a></li>
+        <li><a href="#">Find a Meetup</a></li>        
+        <li><a href="#">Start Your Own Meetup</a></li>
+      </ul>
+    </nav>
     <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<style lang="scss" module>
+.app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	height: 100vh;
+
+	display: flex;
 }
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+.nav {
+	width: 300px;
+	padding: 30px;
+	text-align: left;
+	overflow-y: scroll;
+
+	> ul {
+		margin: 0;
+		padding: 0;
+	}
+
+	ul {
+		list-style: none;
+	}
+
+	a {
+		font-weight: bold;
+		color: #2c3e50;
+		text-decoration: none;
+		&.router-link-exact-active {
+			color: #42b983;
+		}
+	}
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
+  <div class="content">
     <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
-};
+	name: 'home'
+}
 </script>
+
+<style>
+.content {
+	position: relative;
+	padding: 2.2em 0;
+	padding-left: 0px;
+	max-width: 600px;
+	margin: 0 auto;
+	padding-left: 50px;
+}
+</style>
+
