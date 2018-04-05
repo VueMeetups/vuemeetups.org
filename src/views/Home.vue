e99fb73dc7b467fd8ee28a59f2fe75db45ea6fc1
Ben Hong <bencodezen@gmail.com>
2018-04-04 20:14:26 -0400

    feature: Add Getting Started draft content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,62 @@
 <template>
-  <div class="content">
-    <img src="../assets/logo.png">
+  <div class="content guide-with-sidebar index-guide">
+    <h1>Introduction</h1>
+    <h2>Starting Your Own Meetup</h2>
+    <p>That's exciting news! We are glad to see so many people who are enthusiastic about establishing local communities to help Vue enthusiasts and newcomers alike!</p>
+    <p class="tip">Before diving headfirst into how you start your own meetup, be sure to check and make sure there isn't already one in your area! And if there is, we can help put you in contact with the organizers if you'd like to get more involved!</p>
+    <h3>Step 1: Name Your Group</h3>
+    <p>While naming things is often one of the most agonizing parts of starting any project, luckily it's pretty easy for creating a technology oriented meetup.</p>
+    <p>Here are some options:</p>
+    <ul>
+      <li>Vue + YourCity</li>
+      <li>VueJS + YourCity</li>
+      <li>YourCity + Vue</li>
+      <li>YourCity + VueJS</li>
+    </ul>
+    <div class="demo">
+      <li>VueDC</li>
+      <li>Vue.js DC || VueJSDC</li>
+      <li>DC Vue</li>
+      <li>DC Vue.js || DCVueJS</li>
+    </div>
+    <p>You're more than welcome to be more creative, but try to keep it as simple as possible so people can easily find identify it! In addition, don't forget that your name will be critical in the branding step ahead!</p>
+    <h3>Step 2: Grab All The Branding Things!</h3>
+    <p>Now that you've decided on a name, it's time to grab all the branding thingsWhen it comes to getting the word out about your group and the meetups, there's no substitute for having the proper social media tools. There are a few major ones you want to get to make your life easier:</p>
+    <ul>
+      <li>Google (for email and potential YouTube possibilities)</li>
+      <li>Twitter</li>
+    </ul>
+    <p>Some other ones you that are nice to have, but not as critical:</p>
+    <ul>
+      <li>Event Registration (i.e., Meetup / Nvite)</li>
+      <li>Domain Name</li>
+      <li>Instagram</li>
+    </ul>
+    <h3>Step 3: Getting a Venue and Set a Date!</h3>
+    <p>Contrary to popular opinion, you don't need a lineup of speakers in order to start a meetup. The most important thing you actually need is a venue.</p>
+    <p>Some common places that meetups can be held in include:</p>
+    <ul>
+      <li>Your employer's workspace</li>
+      <li>Community centers</li>
+      <li>Co-working spaces</li>
+      <li>College classrooms</li>
+      <li>Local companies who are interested in hosting tech meetups</li>
+    </ul>
+    <h3>Step 4: Meetup Logistics</h3>
+    <p>These are some things that you should consider when planning your meetup:</p>
+    <ul>
+      <li>Does it have a projector? If not, find a TV or spare monitor that could proxy as a projector. And if that's not even an option, upload your slides and let people follow along.</li>
+      <li>Does it have wifi?</li>
+      <li>Does it have easily accessible outlets for people to plug their laptops in during workshops?</li>
+      <li>How accessible is it (i.e., public transportation, parking, etc.)?</li>
+    </ul>
+    <p>And as far as food goes, here are some general guidelines:</p>
+    <ul>
+      <li>Since Meetup events are free, you should account for 30 to 40% of the RSVPs</li>
+      <li>Unless you are serving food that merits a meal (like pizza), please do not say "Free food and drinks will be served" because attendees will be expecting something much heartier than some chips and cookies</li>
+    </ul>
+    <h3>Step 5: Spread the Word!</h3>
+    <p>With your newly powers of social media, make sure to send out your event details on all the social media things! Hashtag #VueJS and #VueJSMeetups to maximize your visibility and we'll make sure to help spread the word!</p>
   </div>
 </template>
 
@@ -10,14 +66,4 @@ export default {
 }
 </script>
 
-<style>
-.content {
-	position: relative;
-	padding: 2.2em 0;
-	padding-left: 0px;
-	max-width: 600px;
-	margin: 0 auto;
-	padding-left: 50px;
-}
-</style>
 
