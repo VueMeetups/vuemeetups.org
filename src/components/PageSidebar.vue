5425f3e590d581f0e6ee036071058404936b4b73
Ben Hong <bencodezen@gmail.com>
2018-04-04 20:14:03 -0400

    feature: Add outline of possible navigation model

--- src/components/PageSidebar.vue
+++ src/components/PageSidebar.vue
@@ -8,15 +8,132 @@ export default {
 <div class="sidebar">
   <div class="sidebar-inner">
     <div class="list">
+      <h2>Community Guide</h2>
       <ul class="menu-root">
         <li>
-          <a href="#">Introduction</a>
+          <h3>Find Your Local Meetup</h3>
         </li>
         <li>
-          <a href="#">Find a Meetup</a>
+          <a class="sidebar-link" href="#">North America</a>
+          <ul class="menu-sub">
+            <li>
+              <a href="#" class="sidebar-link">East Coast</a>
+            </li>
+            <li>
+              <a href="#" class="sidebar-link">Midwest</a>
+            </li>
+            <li>
+              <a href="#" class="sidebar-link">South</a>
+            </li>
+            <li>
+              <a href="#" class="sidebar-link">West Coast</a>
+            </li>
+          </ul>
         </li>
         <li>
-          <a href="#">Start Your Own Meetup</a>
+          <a class="sidebar-link" href="#">Europe</a>
+          <ul class="menu-sub">
+            <li>
+              <a href="#" class="sidebar-link">Northern</a>
+            </li>
+            <li>
+              <a href="#" class="sidebar-link">Western</a>
+            </li>
+            <li>
+              <a href="#" class="sidebar-link">Eastern</a>
+            </li>
+            <li>
+              <a href="#" class="sidebar-link">Central</a>
+            </li>
+            <li>
+              <a href="#" class="sidebar-link">Southern</a>
+            </li>
+            <li>
+              <a href="#" class="sidebar-link">Southeastern</a>
+            </li>
+            <li>
+              <a href="#" class="sidebar-link">Southwestern</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a class="sidebar-link" href="#">Asia</a>
+          <ul class="menu-sub">
+            <li>
+              <a href="#" class="sidebar-link">North</a>
+            </li>
+            <li>
+              <a href="#" class="sidebar-link">East</a>
+            </li>
+            <li>
+              <a href="#" class="sidebar-link">Central</a>
+            </li>
+            <li>
+              <a href="#" class="sidebar-link">South</a>
+            </li>
+            <li>
+              <a href="#" class="sidebar-link">Southweast</a>
+            </li>
+            <li>
+              <a href="#" class="sidebar-link">Southwest</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <h3>Starting Your Own Meetup</h3>
+        </li>
+        <li>
+          <a href="#" class="sidebar-link current">Introduction</a>
+          <ul class="menu-sub current">
+            <li>
+              <a class="sidebar-link current" href="#">Starting Your Own Meetup</a>
+              <ul>
+                <li>
+                  <a class="section-link" href="#">Step 1: Name Your Group</a>
+                </li>
+                <li>
+                  <a class="section-link" href="#">Step 2: Grab All The Branding Things</a>
+                </li>
+                <li>
+                  <a class="section-link" href="#">Step 3: Getting a Venue & Setting a Date</a>
+                </li>
+                <li>
+                  <a class="section-link" href="#">Step 4: Meetup Logistics</a>
+                </li>
+                <li>
+                  <a class="section-link" href="#">Step 5: Spread the Word!</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="#" class="sidebar-link">Resources</a>
+          <ul class="menu-sub">
+              <li>
+                <a class="sidebar-link" href="#">Suggested Topics</a>
+              </li>
+              <li>
+                <a class="sidebar-link" href="#">Templates</a>
+                <ul>
+                  <li class="sidebar-link">Topics</li>
+                  <li class="sidebar-link">Slides</li>
+                  <li class="sidebar-link">Workshops</li>
+                </ul>
+              </li>
+            </ul>
+        </li>
+        <li>
+          <a href="#" class="sidebar-link">Sponsors</a>
+        </li>
+        <li>
+          <a href="#" class="sidebar-link">Reimbursement</a>
+        </li>
+        <li>
+          <a href="#" class="sidebar-link">FAQs</a>
+        </li>
+        <li>
+          <a href="#" class="sidebar-link">Contact</a>
         </li>
       </ul>
     </div>
