bf0b9e26aabc15aa61e8922a16b02c79a400bb8a
Aurore Trunelle <aurore.trunelle@gmail.com>
2018-10-18 13:43:10 +0100

    chore(events): move all events to json data and split upcoming and past compare to today's date

---
+++ docs/.vuepress/components/EventsTimeline.vue
@@ -0,0 +1,56 @@
+<template>
+	<div>
+		<slot/>
+		<template v-for="(timeline, year) in events">
+			<h3>{{ year }}</h3>
+			<template
+				v-for="(events, month) in timeline"
+				v-if="events.length">
+				<h4>{{ month }}</h4>
+				<ul>
+					<li  v-for="event in events">
+					{{ month }} {{event.date }} {{ event.time }} <template v-if="event.organiser">by <a :href="event.organiserLink">{{ event.organiser }}</a></template> - <a :href="event.eventLink">{{ event.name }}</a>
+					</li>
+				</ul>
+			</template>
+		</template>
+	</div>
+</template>
+
+<script>
+import eventsTimeline from '../data';
+import { isPast } from '../utils';
+
+export default {
+	props: {
+		past: {
+			type: Boolean,
+			default: false
+		}
+	},
+
+	computed: {
+		events() {
+			const events = {};
+
+			Object.keys(eventsTimeline).forEach((year) => {
+				Object.keys(eventsTimeline[year]).forEach((month) => {
+					eventsTimeline[year][month].forEach((event) => {
+						if (isPast(event.endDate) === this.past) {
+							if (!events[year]) {
+								events[year] = {};
+							}
+							if (!events[year][month]) {
+								events[year][month] = [];
+							}
+							events[year][month].push(event);
+						}
+					});
+				});
+			});
+
+			return events;
+		}
+	}
+}
+</script>
